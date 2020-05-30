bf7c9c599906e43e8d2f2d1a3137e74bb3d02278
Daniel Alves <daniel@wedapp.io>
2020-05-30 12:37:40 -0400

    step2 - creating the Xcode project

---
+++ project1-1/project1-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project1-1
+//
+//  Created by Weda Alves on 30-05-20.
+//  Copyright © 2020 wedapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ project1-1/project1-1Tests/project1_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  project1_1Tests.swift
+//  project1-1Tests
+//
+//  Created by Weda Alves on 30-05-20.
+//  Copyright © 2020 wedapp. All rights reserved.
+//
+
+import XCTest
+@testable import project1_1
+
+class project1_1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
